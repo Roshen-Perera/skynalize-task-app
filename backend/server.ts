2558d5029e21388fa0a23a8499305723f4c9ba70
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-02-27 19:58:19 +0530

    API is functional

---
+++ backend/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+    origin: "http://localhost:8081",  // Allow frontend requests
+    methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    credentials: true
+}));
+
+
+
+app.listen(3000, (err => {
+    console.log("Server running on port 3000");
+}));
+
+app.use('/health', (req, res, next) => {
+    res.status(200).send('API is functional');
+})
